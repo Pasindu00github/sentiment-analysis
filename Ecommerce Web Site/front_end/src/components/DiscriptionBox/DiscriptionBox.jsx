5331df43ad22ac3d5b7a7ffb264fdb6bf6c0f4fa
Pasindu Jayawardena <pjayawardena349@gmail.com>
2024-03-04 14:22:22 +0530

    Added all components

---
+++ Ecommerce Web Site/front_end/src/components/DiscriptionBox/DiscriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DiscriptionBox.css'
+
+
+const DiscriptionBox = () => {
+  return (
+    <div className='discriptionbox'> 
+      <div className="discriptionbox-navigater">
+        <div className="discriptionbox-nav-box">Description</div>
+        <div className="discriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="discriptionbox-description">
+        <p>An ecommerce clothing site is an online platform where customers can browse and purchase clothing items. These websites typically offer a wide range of products including shirts, pants, dresses, shoes, and accessories from various brands. Users can navigate through different categories, view product details, select sizes and colors, and make purchases directly through the website. Additionally, many ecommerce clothing sites provide features such as customer reviews, sizing guides, and secure payment options to enhance the shopping experience.</p>
+      </div>
+    </div>
+  )
+}
+
+export default DiscriptionBox
