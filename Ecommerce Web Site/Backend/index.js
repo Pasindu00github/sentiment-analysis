46a917e4fa95a9f6c6300d429bba702dc719a582
Pasindu Jayawardena <pjayawardena349@gmail.com>
2024-03-31 13:10:27 +0530

    back end
    back end part 3

--- Ecommerce Web Site/Backend/index.js
+++ Ecommerce Web Site/Backend/index.js
@@ -131,7 +131,87 @@ app.get('/allproducts',async(req,res)=>{
 
 
 })
+// Schema creating for users modle
 
+const Users = mongoose.model('Users',{
+    name:{
+    
+        type:String,
+        
+    },
+    email:{
+        type:String,
+        unique:true,
+
+    },
+    password:{
+        type:String,
+    },
+    cartData:{
+        type:Object,
+    },
+    data:{
+        type:Date,
+        default:Date.now,
+
+    }
+})
+
+
+// Careating Endpoin for registering users
+
+app.post('/signup',async(req,res)=>{
+
+    let check = await Users.findOne({email:req.body.email});
+    if(check){
+        return res.status(400).json({success:false,errors:"Exsisting user found with same email Address"})
+    }
+    let cart ={};
+    for(let i = 0; i < 300; i++){
+        cart[i]=0;
+    }
+    const user =new Users({
+        name:req.body.username,
+        email:req.body.email,
+        password:req.body.password,
+        cartData:cart,
+    }) 
+
+    await user.save();
+
+    const data ={
+        user:{
+            id:user.id
+        }
+    }
+
+    const token =jwt.sign(data,'secret_ecom');
+    res.json({success:true,token})
+})
+
+// creating endpoint userlogin
+app.post('/login',async(req,res)=>{
+     let user = await Users.findOne({email:req.body.email});
+     if(user){
+        const passCompare = req.body.password === user.password;
+        if(passCompare){
+            const data= {
+                user:{
+                    id:user.id
+                }
+
+            }
+            const token =jwt.sign(data,'secret_ecom');
+            res.json({success:true,token});
+        }
+        else{
+            res.json({success:false,errors:'Wrong Password'});
+        }
+     }
+     else{
+        res.json({success:false,errors:"Wrong Email ID"});
+     }
+})
 
 app.listen(port,(error)=>{
     if(!error){
